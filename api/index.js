b39b4fa67c0a182059c4f1549e1b5d4932900687
Tuấn Vũ <tuanvundvn@gmail.com>
2020-11-20 22:36:11 +0700

    changed

--- api/index.js
+++ api/index.js
@@ -1,5 +1,30 @@
-module.exports = (req, res) => {
-    res.json({
-        msg: "helo",
+import Cors from "cors";
+
+// Initializing the cors middleware
+const cors = Cors({
+    methods: ["GET", "HEAD"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result) => {
+            if (result instanceof Error) {
+                return reject(result);
+            }
+            return resolve(result);
+        });
     });
-};
+}
+
+async function handler(req, res) {
+    // Run the middleware
+    await runMiddleware(req, res, cors);
+
+    // Rest of the API logic
+    res.setHeader("Set-Cookie", cookie.serialize("id", "nam"));
+    res.json({ message: "Hello Everyone!" });
+}
+
+export default handler;
